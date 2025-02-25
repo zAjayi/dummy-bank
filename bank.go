5cd6280210a3300f25923f75a283a46342c41a46
zAjayi <zemirajayi2805@gmail.com>
2025-02-25 16:30:00 +0100

    my commit

--- bank.go
+++ bank.go
@@ -5,4 +5,16 @@ import "fmt"
 func main() {
 	fmt.Println("Welcom to Go Bank.")
 	fmt.Println("What would you like to do?")
+	fmt.Println("1. Check balance")
+	fmt.Println("2. Deposit money")
+	fmt.Println("3. Withdraw money")
+	fmt.Println("4. Exit")
+
+	var choice int
+	fmt.Print("What is your choice? ")
+	fmt.Scan(&choice)
+
+	
+
+	fmt.Println("Your choice is ", choice)
 }
